e37dc1b08b127099bb8813b7d981c0cddce25c16
alyson.ramos <ramos.alysonfarias@gmail.com>
2024-08-06 12:58:38 -0300

    feat: PaginationSelector

--- src/components/PaginationSelector.tsx
+++ src/components/PaginationSelector.tsx
@@ -5,4 +5,50 @@ import {
   PaginationLink,
   PaginationNext,
   PaginationPrevious,
-} from "./ui/pagination";
+} from "./ui/Pagination";
+
+type Props = {
+  page: number;
+  pages: number;
+  onPageChange: (page: number) => void;
+};
+
+const PaginationSelector = ({ page, pages, onPageChange }: Props) => {
+  const pageNumbers = [];
+  for (let i = 1; i <= pages; i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <Pagination>
+      <PaginationContent>
+        {page !== 1 && (
+          <PaginationItem>
+            <PaginationPrevious
+              href="#"
+              onClick={() => onPageChange(page - 1)}
+            />
+          </PaginationItem>
+        )}
+
+        {pageNumbers.map((number) => (
+          <PaginationItem>
+            <PaginationLink
+              href="#"
+              onClick={() => onPageChange(number)}
+              isActive={page === number}
+            >
+              {number}
+            </PaginationLink>
+          </PaginationItem>
+        ))}
+
+        {page !== pageNumbers.length && (
+          <PaginationItem>
+            <PaginationNext href="#" onClick={() => onPageChange(page + 1)} />
+          </PaginationItem>
+        )}
+      </PaginationContent>
+    </Pagination>
+  );
+};
