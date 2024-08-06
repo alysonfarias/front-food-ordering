ad8959a6535465d87352ac8380dcfe18443afbb1
alyson.ramos <ramos.alysonfarias@gmail.com>
2024-08-06 17:59:32 -0300

    feat: appRoutes

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,6 +1,13 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./layouts/layout";
 import HomePage from "./pages/HomePage";
+import SearchPage from "./pages/SearchPage";
+import AuthCallbackPage from "./pages/AuthCallbackPage";
+import ProtectedRoute from "./auth/ProtectedRoute";
+import DetailPage from "./pages/DetailPage";
+import OrderStatusPage from "./pages/OrderStatusPage";
+import UserProfilePage from "./pages/UserProfilePage";
+import ManageRestaurantPage from "./pages/ManageRestaurantPage";
 
 const AppRoutes = () => {
   return (
@@ -13,6 +20,51 @@ const AppRoutes = () => {
           </Layout>
         }
       />
+      <Route path="*" element={<Navigate to="/" />} />
+      <Route path="/auth-callback" element={<AuthCallbackPage />} />
+      <Route
+        path="/search/:city"
+        element={
+          <Layout showHero={false}>
+            <SearchPage />
+          </Layout>
+        }
+      />
+      <Route
+        path="/detail/:restaurantId"
+        element={
+          <Layout showHero={false}>
+            <DetailPage />
+          </Layout>
+        }
+      />
+      <Route element={<ProtectedRoute />}>
+        <Route
+          path="/order-status"
+          element={
+            <Layout>
+              <OrderStatusPage />
+            </Layout>
+          }
+        />
+        <Route
+          path="/user-profile"
+          element={
+            <Layout>
+              <UserProfilePage />
+            </Layout>
+          }
+        />
+        <Route
+          path="/manage-restaurant"
+          element={
+            <Layout>
+              <ManageRestaurantPage />
+            </Layout>
+          }
+        />
+      </Route>
+
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
