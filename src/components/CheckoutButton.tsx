7ea0c1481fdc41dd7750d49b4dea1e59097002d6
alyson.ramos <ramos.alysonfarias@gmail.com>
2024-08-10 10:35:00 -0300

    refactor: full path

--- src/components/CheckoutButton.tsx
+++ src/components/CheckoutButton.tsx
@@ -2,7 +2,11 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { useLocation } from "react-router-dom";
 import { Button } from "./ui/button";
 import LoadingButton from "./LoadingButton";
-import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+} from "src/components/ui/dialog.tsx";
 import UserProfileForm, {
   UserFormData,
 } from "@/forms/user-profile-form/UserProfileForm";
